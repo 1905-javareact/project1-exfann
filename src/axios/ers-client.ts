75defeaef5149e634c108fb0416828ffe945789e
Eva Fann <exfann@gmail.com>
2019-06-07 12:27:57 -0400

    axiospls

--- src/axios/ers-client.ts
+++ src/axios/ers-client.ts
@@ -4,7 +4,7 @@ import axios from 'axios'
 //base http request
 //please work
 export const ersClient = axios.create({
-    baseURL: 'http://ec2-18-220-25-148.us-east-2.compute.amazonaws.com:9050',
+    baseURL: 'https://ec2-18-220-25-148.us-east-2.compute.amazonaws.com:9050',
     headers:{
         'Content-Type': 'application/json'
     },
